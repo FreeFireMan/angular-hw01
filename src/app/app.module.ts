b6edc77018213ab66d9f7667226ee9a888e6e508
andrey-kupets <andrey.kupets@i.ua>
2021-01-19 02:32:35 +0300

    render cars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
